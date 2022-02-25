8b7fa46c79dd23b247eb5debaf68fccf1f7e7a6a
Rustam <74149602+mudarib@users.noreply.github.com>
2022-02-25 15:16:07 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by macbook on 25.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
