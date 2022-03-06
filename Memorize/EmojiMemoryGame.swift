042cca1495299ad8fdd43da80ea67e7a0465a9e1
Rustam <74149602+mudarib@users.noreply.github.com>
2022-03-06 17:56:13 +0300

    first commit

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiMemotyGame.swift
+//  Memorize
+//
+//  Created by macbook on 04.03.2022.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    static let emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+                MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+                        EmojiMemoryGame.emojis[pairIndex]
+                    }
+            }
+        
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by macbook on 04.03.2022.
+//
+
+import Foundation
